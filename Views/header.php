bd112a03628763665df98c637135880689f765d9
Julian Castro <julmdq@live.com.ar>
2021-10-11 00:37:41 -0300

    Icono + Titulo

--- Views/header.php
+++ Views/header.php
@@ -8,7 +8,9 @@
      <!-- Bootstrap CSS -->
      <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>bootstrap.min.css">
      <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>estilos.css">
+     <link rel="shortcut icon" type="image/x-icon" href= "<?php echo FRONT_ROOT.VIEWS_PATH ?>img/briefcase.png"  />
 
-     <title>TP FINAL</title>
+
+     <title>Bolsa Laboral UTN</title>
 </head>
 <body>
