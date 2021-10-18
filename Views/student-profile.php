6a2215982863b48182641d9a03bcee4151bdc38b
Julian Castro <julmdq@live.com.ar>
2021-10-17 21:28:47 -0300

    Vista estudiante

--- Views/student-profile.php
+++ Views/student-profile.php
@@ -2,7 +2,8 @@
     require_once('nav.php');
 ?>
 
-<div class="card" style="width: 18rem;">
+<center>
+<div class="card" style="width: 27rem; margin-top: 5rem;">
   <img src= <?php echo FRONT_ROOT.VIEWS_PATH ?>img/user.png ?>
   <div class="card-body">
     <h5 class="card-title">Informacion del Alumno</h5>
@@ -17,6 +18,13 @@
           <li class="list-group-item">Nombre: <?php echo $student->getFirstName(); ?></li>
           <li class="list-group-item">Apellido: <?php echo $student->getLastName(); ?></li>
           <li class="list-group-item">DNI: <?php echo $student->getDni(); ?></li>
+          <li class="list-group-item">Legajo: <?php echo $student->getFileNumber(); ?></li>
+          <li class="list-group-item">Genero: <?php echo $student->getGender(); ?></li>
+          <li class="list-group-item">Fecha de Nacimiento: <?php echo $student->getBirthDate(); ?></li>
+          <li class="list-group-item">Email: <?php echo $student->getEmail(); ?></li>
+          <li class="list-group-item">Telefono: <?php echo $student->getPhoneNumber(); ?></li>
+ 
+
           <?php
         }
       }
@@ -24,3 +32,4 @@
     
   </ul>
 </div>
+</center>
