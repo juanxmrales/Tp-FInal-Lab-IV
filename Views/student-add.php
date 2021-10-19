4b5f9830721979275bb0e2238e236780ade959d4
PablogRizzo <pablog.rizzo@gmail.com>
2021-10-18 23:07:39 -0300

    Enter

--- Views/student-add.php
+++ Views/student-add.php
@@ -3,7 +3,7 @@
 ?>
 <main class="py-5">
      <section id="listado" class="mb-5">
-          <div class="container">
+          <div class="container pt-5">
                <h2 class="mb-4">Agregar alumno</h2>
                <form action="<?php echo FRONT_ROOT ?>Student/Add" method="post" class="bg-light-alpha p-5">
                     <div class="row">                         
