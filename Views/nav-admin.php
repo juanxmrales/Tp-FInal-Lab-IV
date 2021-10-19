90a9433a788784de799a4f774433c21174cb2115
PablogRizzo <pablog.rizzo@gmail.com>
2021-10-18 21:16:28 -0300

    Enter

--- Views/nav-admin.php
+++ Views/nav-admin.php
@@ -1,23 +1,32 @@
-<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
-     <a class="navbar-brand" href="index.php">
-          <img src= <?php echo FRONT_ROOT.VIEWS_PATH ?>img/admin.png ?>
-     </a>
-     
-     <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/SearchStudent">Listar Alumnos</a>
-          </li>     
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/ShowListView">Listar de Empresas</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/ShowAddView">Agregar Empresa</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>JobPosition/ShowAddView">Agregar oferta</a>
-          </li>
-          <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>User/LogOut">Log out</a>
-          </li>     
-     </ul>
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark fixed-top">
+     <div class="container">
+          <a class="navbar-brand" href="index.php">
+               <img src= <?php echo FRONT_ROOT.VIEWS_PATH ?>img/admin.png ?>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+               <ul class="navbar-nav">
+                    <li class="nav-item">
+                         <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/SearchStudent">Listar Alumnos</a>
+                    </li>     
+                    <li class="nav-item">
+                         <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/ShowListView">Listar de Empresas</a>
+                    </li>
+                    <li class="nav-item">
+                         <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/ShowAddView">Agregar Empresa</a>
+                    </li>
+                    <li class="nav-item">
+                         <a class="nav-link" href="<?php echo FRONT_ROOT ?>JobPosition/ShowAddView">Agregar oferta</a>
+                    </li>
+                    <li class="nav-item">
+                         <a class="nav-link" href="<?php echo FRONT_ROOT ?>JobPosition/ShowListView">Mostrar ofertas</a>
+                    </li>
+                    <li class="nav-item">
+                         <a class="nav-link" href="<?php echo FRONT_ROOT ?>User/LogOut">Log out</a>
+                    </li>     
+               </ul>
+          </div>
+     </div>
 </nav>
