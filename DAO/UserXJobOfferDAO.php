48c2743635a49ff8ad795093c3967a3a0390e257
Agustín Paz <colilo40@gmail.com>
2021-10-28 21:15:54 -0300

    UserXJobOffer

--- DAO/UserXJobOfferDAO.php
+++ DAO/UserXJobOfferDAO.php
@@ -1,5 +1,59 @@
 <?php
 
+    namespace DAO;
     
+    use \Exception as Exception;
+    use Models\UserXJobOffer;
 
+    class UserXJobOfferDAO
+    {
+        private $connection;
+        private $tableName = "usersxjoboffers";
+
+        public function Add(UserXJobOffer $userXJob)
+        {
+            try
+            {
+                $query = "INSERT INTO ".$this->tableName." (id_usuario, id_jobOffer) VALUES (:password, :type);";
+                
+                $parameters["id_usuario"] = $userXJob->getUserId();
+                $parameters["id_jobOffer"] = $userXJob->getJobOfferId();
+
+                $this->connection = Connection::GetInstance();
+
+                $this->connection->ExecuteNonQuery($query, $parameters);
+            }
+            catch(Exception $ex)
+            {
+                throw $ex;
+            }
+        }
+
+        public function GetAll()
+        {
+            try
+            {
+                $list = array();
+
+                $query = "SELECT * FROM ".$this->tableName;
+
+                $this->connection = Connection::GetInstance();
+
+                $resultSet = $this->connection->Execute($query);
+                
+                foreach ($resultSet as $row)
+                {                
+                    $userXJob = new UserXJobOffer($row["id_usuario"],$row["id_jobOffer"]);
+
+                    array_push($list, $userXJob);
+                }
+
+                return $list;
+            }
+            catch(Exception $ex)
+            {
+                throw $ex;
+            }
+        }
+    }
 ?>
---
+++ Models/UserXJobOffer.php
@@ -0,0 +1,57 @@
+<?php
+
+    namespace Models;
+
+    class UserXJobOffer
+    {
+        private $userId;
+        private $jobOfferId;
+
+        public function __construct($userId,$jobOfferId)
+        {
+            $this->userId = $userId;
+            $this->jobOfferId = $jobOfferId;
+        }
+
+        /**
+         * Get the value of userId
+         */ 
+        public function getUserId()
+        {
+                return $this->userId;
+        }
+
+        /**
+         * Set the value of userId
+         *
+         * @return  self
+         */ 
+        public function setUserId($userId)
+        {
+                $this->userId = $userId;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of jobOfferId
+         */ 
+        public function getJobOfferId()
+        {
+                return $this->jobOfferId;
+        }
+
+        /**
+         * Set the value of jobOfferId
+         *
+         * @return  self
+         */ 
+        public function setJobOfferId($jobOfferId)
+        {
+                $this->jobOfferId = $jobOfferId;
+
+                return $this;
+        }
+    }
+
+?>
