b911596f52285f6651b361d4232a66c885503225
juanxmrales <juanxmrales@gmail.com>
2021-11-03 00:36:31 -0300

    footer

--- Views/footer.php
+++ Views/footer.php
@@ -2,15 +2,29 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/b4c4dc37b0.js" crossorigin="anonymous"></script>
 
-<footer>
-	<div class="footer-item">
-		<h3>About us</h3>
-		<i class="fas fa-at"></i><a href="mailto:pablog.rizzo@gmail.com">Email Agus Paz</a>
-		<i class="fas fa-at"></i>
-        <a href="mailto:pablog.rizzo@gmail.com">Email Julian Castro</a>
-		<i class="fas fa-at"></i>
-        <a href="mailto:pablog.rizzo@gmail.com">Email Juan Cruz Morales</a>
-		<i class="fas fa-at"></i>
-        <a href="mailto:pablog.rizzo@gmail.com">Email Pablo Rizzo</a>
-	</div>
-</footer>
+
+/*<footer>
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>company</h4>
+  	 				informacion sobre la empresa
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Contacto</h4>
+  	 			<ul>
+				   <li><i class="fas fa-at"></i><a href="mailto:pablog.rizzo@gmail.com">Email Agus Paz</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+</footer>*/
